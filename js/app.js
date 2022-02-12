c01270f44066c6b24f1c877ca074723a133518ad
Agnes <almoha@gmail.com>
2022-02-12 19:04:00 +0100

    modale

--- js/app.js
+++ js/app.js
@@ -24,3 +24,15 @@ function complete(){
     clearInterval(timer);
     timer = null;
 }
+
+//MODALE
+
+const modalContainer = document.querySelector('.modal-container');
+const modalTriggers = document.querySelectorAll('.modal-trigger');
+
+modalTriggers.forEach(trigger => trigger.addEventListener('click', toggleModal));
+
+function toggleModal(){
+    modalContainer.classList.toggle("active");
+}
+    
